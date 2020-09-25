ca9cbfe575757bd7931735559585dd87e382d740
bbajpai <b.bhumi101@gmail.com>
2020-09-25 09:06:13 +0530

    A

--- firstapp/src/component/Home.js
+++ firstapp/src/component/Home.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React,{Component} from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import JSON from '../db.json';
 
-const Home = () => {
-    return(
-        <React.Fragment>
-            <Header/>
-            <h1>React app</h1>
-            <h2>Developer Funnel</h2>
-            <Footer year="2021" month="Sep"/>
-        </React.Fragment>
+class Home extends Component{
+    constructor(){
+        super()
 
-    )
+        this.state={
+            news:JSON
+        }
+    }
+
+    render(){
+        console.log(this.state.news)
+        return(
+            <React.Fragment>
+                <Header/>
+                <h1>React app</h1>
+                <h2>Developer Funnel</h2>
+                <Footer year="2021" month="Sep"/>
+            </React.Fragment>
+        )
+    }
 }
+
 
 export default Home;
